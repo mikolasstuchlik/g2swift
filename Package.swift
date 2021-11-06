4050181b8db937c03fedd4271bfc50c71d0ed5d8
Mikoláš Stuchlík <stuchlej@gmail.com>
2021-11-06 14:28:52 +0100

    feautre: Implement POC usage of covfefe parser

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/jpsim/SourceKitten", .branch("master")),
-        .package(url: "https://github.com/palle-k/Covfefe.git", .branch("develop"))
+        .package(url: "https://github.com/mikolasstuchlik/Covfefe.git", .branch("fix/stack-overflow"))
     ],
     targets: [
         .target(
--- Sources/libg2swift/RNCInterpreter/RNCTokenizer.swift
+++ Sources/libg2swift/RNCInterpreter/RNCTokenizer.swift
@@ -3,17 +3,86 @@ import Covfefe
 
 public enum RNCTokenizer {
     public static func tokenize(_ string: String) throws {
-        let grammar = Grammar(start: "initial") {
-            "initial"       --> n("initial") <+> t(.illegalCharacters.inverted)
-                            <|> t()
+
+        // RESPECT trailing spaces!!!!
+        let grammar = Grammar(start: "grammar") {
+            "grammar"           --> t("grammar { ") <+> n("grammar.content") <+> t("}")
+
+            "grammar.content"   --> n("word") <+> n("grammar.content")
+                                <|> t()
+
+            "word"              --> n("string") <+> t(" ")
+            "string"            --> t(.illegalCharacters.inverted.subtracting(.whitespacesAndNewlines)) <+> n("string")
+                                <|> t()
         }
 
         let parser = EarleyParser(grammar: grammar)
 
-        print(string.count)
-        // 2017 -> OK / FAIL
-        // 2018 -> FAIL
-        let syntaxTree = try parser.syntaxTree(for: String(string.prefix(20)))
-        print(syntaxTree.description)
+        let syntaxTree = try parser.syntaxTree(for: string)
+
+        let words: [String] = syntaxTree.reduce([]) { current, accumulator in
+            if case let .node(key: node, children: _) =  current, node.name == "word" {
+                let leafs = current.stackLeafs
+                let word = string[leafs.first!.lowerBound..<leafs.last!.upperBound]
+                accumulator.append(String(word))
+                return false
+            }
+
+            return true
+        }
+
+        print(words)
+    }
+}
+
+extension SyntaxTree {
+    
+    private enum ReduceStackFrame {
+        case children([SyntaxTree], index: Int)
+    }
+
+    func reduce<T>(_ initial: T, nextContinueSubtree: (_ currentItem: SyntaxTree, _ result: inout T) -> Bool ) -> T {
+        var stack = [ReduceStackFrame]()
+        var accumulator = initial
+
+        func appendNew(_ tree: SyntaxTree) {
+            if 
+                nextContinueSubtree(tree, &accumulator),
+                case .node(key: _, children: let children) = tree 
+            {
+                stack.append(.children(children, index: 0))
+            }
+        }
+
+        func resolve(_ children: [SyntaxTree], iteratedIndex: Int) {
+            guard children.count > iteratedIndex else {
+                return
+            } 
+
+            stack.append(.children(children, index: iteratedIndex + 1))
+            appendNew(children[iteratedIndex])
+        }
+
+        appendNew(self)
+        while let currentFrame = stack.popLast() {
+            switch currentFrame {
+            case let .children(children, index: index):
+                resolve(children, iteratedIndex: index)
+            }
+        }
+	
+        return accumulator
+    }
+
+}
+
+extension SyntaxTree {
+    var stackLeafs: [LeafElement] {
+         self.reduce([]) { current, accumulator in
+            if case let .leaf(leaf) = current {
+                accumulator.append(leaf)
+            }
+            return true
+        }
     }
 }
